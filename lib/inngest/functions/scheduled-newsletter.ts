26e26f205e69a42ab93396b9cfaedb8f7d269a69
excel <excelyynxl@gmail.com>
2026-01-07 09:48:37 +0800

    ingngest scheduling newsletter route

--- lib/inngest/functions/scheduled-newsletter.ts
+++ lib/inngest/functions/scheduled-newsletter.ts
@@ -1,12 +1,42 @@
 import { fetchArticles } from "@/lib/news";
 import { inngest } from "../client";
-
+import { marked } from "marked";
+import { sendEmail } from "@/lib/email";
+import { createClient } from "@/lib/server";
 
 export default inngest.createFunction(
-    {id: "newsletter/scheduled"}, 
+    {
+        id: "newsletter/scheduled", 
+        cancelOn: [
+            {
+                event: "newsletter.schedule.deleted",
+                if: "async.data.userId == event.data.userId",
+            },
+        ]
+    }, 
     {event: "newsletter.schedule"},
     async ({event, step, runId}) => {
-        const categories = ["technologies", "buisiness", "politics"];
+
+        const isUserActive = await step.run("check-user-status", async () => {
+            const supabase = await createClient();
+            const { data, error } = await supabase
+                .from("user_preferences")
+                .select("is_active")
+                .eq("user_id", event.data.userId)
+                .single();
+             
+            if (error) {
+                return false;
+            }
+
+            return data?.is_active || false;
+        });
+
+        if (!isUserActive){
+            return {};
+        }
+
+        const categories = event.data.categories;
 
         // fetch articles per category
         const allArticles = await step.run("fetch-news", async () => {
@@ -49,8 +79,60 @@ export default inngest.createFunction(
 
         console.log(summary.choices[0].message.content);
 
-        
+        const newsletterContent = summary.choices[0].message.content;
 
-        return {};
+        if (!newsletterContent){
+            throw new Error("Failed to generate newsletter content.");
+        }
+
+        const htmlResult = await marked(newsletterContent);
+
+        await step.run("send-email", async () => {
+            await sendEmail(
+                event.data.email, 
+                event.data.categories.join(", "), 
+                allArticles.length,
+                htmlResult 
+            );
+        });
+
+        await step.run("schedule-next", async () => {
+            const now = new Date();
+
+            let nextScheduleTime: Date;
+
+            switch (event.data.frequency) {
+                case "daily":
+                    nextScheduleTime = new Date(now.getTime() + 24 * 60 * 60 *1000);
+                    break;
+                case "weekly":
+                    nextScheduleTime = new Date(now.getTime() + 7 * 24 * 60 * 60 *1000);
+                    break;
+                case "biweekly":
+                    nextScheduleTime = new Date(now.getTime() + 3 * 24 * 60 * 60 *1000);
+                    break;
+                default:
+                    nextScheduleTime = new Date(now.getTime() + 24 * 60 * 60 *1000);
+            }
+
+            nextScheduleTime.setHours(9, 0, 0, 0); // make sure newsletter is sent out at 9am
+
+            await inngest.send({
+                name: "newsletter.schedule",
+                data: {
+                    categories,
+                    email: event.data.email,
+                    frequency: event.data.frequency,
+                    userId: event.data.userId,
+                },
+                ts: nextScheduleTime.getTime(),
+            });
+        });
+
+        return {
+            newsletter: htmlResult, 
+            articlesCount: allArticles.length, 
+            nextScheduled: true
+        };
     }
 )
